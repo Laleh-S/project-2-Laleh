1f676ca1614cbc879f61eb4820327e37efda7599
DMTA <100156884+Dumblevor@users.noreply.github.com>
2022-05-14 19:15:18 +0300

    small add ons including navbar etc

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,29 @@
-import React from "react"
 import { Link } from "react-router-dom"
 
-function Navbar () {
+import React from "react"
 
+function Navbar() {
   return (
-    <div className="">
-      
-    </div>
+    <>
+      <header>
+        <nav className="navbar is-dark">
+          <div className="container">
+            <div className="navbar-brand">
+              <Link to="/" className="navbar-item">
+                Home
+              </Link>
+              <Link to="/about" className="navbar-item">
+                About
+              </Link>
+              <Link to="/charPreview" className="navbar-item">
+                Your Character
+              </Link>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </>
   )
 }
+
 export default Navbar
