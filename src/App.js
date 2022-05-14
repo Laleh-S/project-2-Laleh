6af17f6dcb4eeab8f8c0470c5715450cad644c6f
DMTA <100156884+Dumblevor@users.noreply.github.com>
2022-05-14 16:53:22 +0300

    set up App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-function App() {
-  return <h1>
-    Hey World.
-  </h1>
-}
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
+import Home from "./components/Home"
+import About from "./components/About"
+import Navbar from "./components/Navbar"
+import ChosenCharacter from "./components/ChosenCharacter"
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/characterdisplay/:ChosenCharacter" element={<ChosenCharacter />} />
+        </Routes>
+      </main>
+    </Router>
+  )
+}
 export default App
