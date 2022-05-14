3d0ae4c776521cd5a1a7188ad710220067c5ba8b
DMTA <100156884+Dumblevor@users.noreply.github.com>
2022-05-14 19:10:05 +0300

    home fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,20 @@
 import React from "react"
 
-function Home () {
-<<<<<<< HEAD
-=======
+function Home() {
 
-function HandleCick(e) {
-  let gender = e.target.value
-  return gender
-}
-
-
-  return(
+  function HandleCick(e) {
+    let gender = e.target.value
+    return gender
+  }
+  return (
     <div>
->>>>>>> 81ffee4b58f116a470055a35817d5bf3d63f2a4e
 
-  return(
-    <div className="">
-      
+      return(
+      <div className="">
+
+      </div>
+      )
     </div>
-  //each button needs value of Gender; each buttons needs to execute HandleClick()
-)
+  )
 }
 export default Home
