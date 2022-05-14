d95cd3b2a002d6b7a6b1a88f57784ef3dc76f0a8
DMTA <100156884+Dumblevor@users.noreply.github.com>
2022-05-14 19:03:04 +0300

    dom and stuff added

--- src/components/DisplayCharacter.js
+++ src/components/DisplayCharacter.js
@@ -1,6 +1,23 @@
 import React from "react"
+import { Link, useParams, useLocation } from "react-router-dom"
+//import Country from "./Country"
+
 
 function DisplayCharacter () {
+  const { genderName } = useParams()
+  const [character, setCharacter] = React.useState(undefined)
+
+
+  React.useEffect(() => {
+    async function fetchCountry() {
+      const resp = await fetch(`https://rickandmortyapi.com/api/character/?gender=${genderName}`)
+      const characterData = await resp.json()
+      setCharacter(characterData[0])
+    }
+    fetchCountry()
+  }, [genderName])
+
+
   return (
     <div>
       
