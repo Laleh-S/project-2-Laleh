dec94f2c1a030c786aff773dab3c5508f4431b16
DMTA <100156884+Dumblevor@users.noreply.github.com>
2022-05-21 13:24:46 +0300

    debugging maybe?

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom"
 import React from "react"
-import randomMorty2 from "../assets/RandomMORTY-2.gif" 
+import randomMorty2 from "../assets/RandomMORTY-2.gif"
 
 function Navbar() {
   return (
     <>
       <header>
         <nav className="navbar is-warning is-spaced has-shadow">
-          <Link to="/"><img  className="logo image image is-64x64" src={randomMorty2} /></Link>
           <div className="container-nav ">
             <div className="navbar-brand">
+              <Link to="/"><img className="logo image image is-64x64" src={randomMorty2} /></Link>
               <Link to="/" className="navbar-item is-size-3 has-text-weight-bold ml-3">
                 Home
               </Link>
